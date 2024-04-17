15bc332de02aae5ee3521fa35a5cf579c1c91267
Sharmake Awil <sharmarke.awil@hotmail.com>
2024-04-17 17:32:07 +0200

    form default

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 // import Button from "./components/Button/Button";
 // import Input from "./components/Input/Input";
-import InputPassword from "./components/Input/InputPassword";
+// import InputPassword from "./components/Input/InputPassword";
+import Form from "./components/Form/Form";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <h1>test</h1>
-      <InputPassword />
+      
+      <Form></Form>
     </>
   );
 }
